9b87f9cdebb52ac186bd3177c40bd03c3d2f9d21
Anand21K <95066767+Anand21K@users.noreply.github.com>
2022-09-27 22:55:02 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Node from './Node';
+
+function App(){
+  return <div>
+    <Header />
+    <Node />
+    <Footer />
+    </div>
+}
+
+export default App;
---
+++ src/components/Node.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Node() {
+  return (
+    <div className="note">
+      <h1>This ia title</h1>
+      <p>This is content</p>
+    </div>
+  );
+}
+
+export default Node;
